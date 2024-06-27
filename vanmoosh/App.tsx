908cb9a9da30785eccd9d4d502efbab181f2eaf0
Matheuss <matheusgida@outlook.com>
2024-06-27 09:16:26 -0300

    chore: Update project configuration and file structure

--- vanmoosh/App.tsx
+++ vanmoosh/App.tsx
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
-import styles_Trip from './src/components/styles/Trip';
+import TripStyles from './src/components/styles/Trip';
 
 export default function App() {
   return (
-    <View style={styles_Trip.container}>
-      <Text>Hello World!</Text>
+    <View style={TripStyles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
