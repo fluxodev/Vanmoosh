752f70ebc97abc991ec29d9737a605618f13814e
Isa <124785311+isacaramigo@users.noreply.github.com>
2024-06-29 18:24:42 -0300

    feat: library fonts

--- vanmoosh/App.tsx
+++ vanmoosh/App.tsx
@@ -1,11 +1,17 @@
 import Groups from '@screens/school/Groups';
 import { ThemeProvider } from 'styled-components'
 import theme from '@theme/index'
-
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { ActivityIndicator } from 'react-native';
 export default function App() {
+
+ const [fontsLoader] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      { fontsLoader ? <Groups /> : <ActivityIndicator/>}
     </ThemeProvider>
   );
 }
