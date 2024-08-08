92a7fd982bd58fd2e84074f955243eeb252fb900
Matheuss <matheusgida@outlook.com>
2024-08-07 22:19:29 -0300

    :wrench: build: .env file

--- vanmoosh/babel.config.js
+++ vanmoosh/babel.config.js
@@ -4,6 +4,12 @@ module.exports = function(api) {
     presets: ['babel-preset-expo'],
     plugins: [
       [
+        'module:react-native-dotenv', {
+          moduleName: '@env',
+          path: '.env',
+        }
+      ],
+      [
         'module-resolver',
         {
           root: ['./src'], //estou indicando a raiz do projeto
