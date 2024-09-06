94d4d4ce64f982477a3b9265ba74e0858b3e4a19
Davi Fontanini <davisamufon@gmail.com>
2024-09-06 00:22:17 -0300

    :sparkles: feat: Cadastro de alunos

--- vanmoosh/src/screens/responsible/AddChild/index.tsx
+++ vanmoosh/src/screens/responsible/AddChild/index.tsx
@@ -7,12 +7,30 @@ import { ButtonAdd } from "@components/Button";
 import { Alert } from "react-native";
 import { addStudentToGroup } from "@libs/firebase/db/students/includeStudentInTheGroup";
 
+import { addChild } from "@libs/firebase/db/Responsibles/addChild";
+import { useState } from "react";
+
 export function AddChild(){
+    const [name, setName] = useState('');
+    const [birthdayDate, setBirthdayDate] = useState('');
+    const [adress, setAdress] = useState('');
+    const [cpf, setCpf] = useState('');
+    const [anoEscolar, setAnoEscolar] = useState('');
+    const [groupCode, setGroupCode] = useState('');
+
 
     async function handleOnButton() {
-        
-        const groupCode = '645180'
-        await addStudentToGroup(groupCode)
+        try {
+            const result = await addChild({name, birthdayDate, adress, cpf, anoEscolar})
+            if (!result) {
+                console.log("Erro: usuário não cadastrado")
+                return
+            }
+            await addStudentToGroup(groupCode)
+        } catch (error) {
+            console.error("Erro ao registrar: ", error)
+        }
+
         
     }
 
@@ -31,26 +49,39 @@ export function AddChild(){
                     placeholder="Nome Completo"
                     returnKeyType="done" //Muda o "Retorno" para "Concluido".
                     autoCorrect= {false}
+                    value={name}
+                    onChangeText={setName}
                 />
+
                 <Input 
                     placeholder="CPF"
-                    returnKeyType="done"  
+                    returnKeyType="done"
+                    value={cpf}
+                    onChangeText={setCpf}
                 />
+                
                 <Input 
                     placeholder="Data de Nascimento"
-                    returnKeyType="done"  
+                    returnKeyType="done"
+                    value={birthdayDate}
+                    onChangeText={setBirthdayDate}
                 />
 
                 <Input  
                     placeholder="Ano Escolar"
                     returnKeyType="done"
                     autoCorrect= {false}
+                    value={anoEscolar}
+                    onChangeText={setAnoEscolar}
                 />
 
                 <Input  
                     placeholder="Código da Sala"
                     returnKeyType="done"
                     autoCorrect= {false}
+                    value={groupCode}
+                    onChangeText={setGroupCode}
+
                 />
                 <Box />
 
